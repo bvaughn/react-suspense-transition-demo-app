6fafb4ab6a93bde02aee67fcb71ca93661bd89cf
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-05-09 11:07:24 -0400

    Demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,146 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense, useDeferredValue, useState, useTransition } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [defaultCount, setDefaultCount] = useState(0);
+  const [transitionCount, setTransitionCount] = useState(0);
+  const deferredCount = useDeferredValue(defaultCount);
+
+  const [mounted, setMounted] = useState(false);
+  const [updateType, setUpdateType] = useState<
+    "update" | "transition" | "deferred"
+  >("update");
+
+  const [isPending, startTransition] = useTransition();
+
+  const mount = () => setMounted(true);
+  const update = () => {
+    setDefaultCount(defaultCount + 1);
+    setTransitionCount(defaultCount + 1);
+    setUpdateType("update");
+  };
+  const updateTransition = () => {
+    startTransition(() => {
+      setTransitionCount(count + 1);
+    });
+    setDefaultCount(defaultCount + 1);
+    setUpdateType("transition");
+  };
+  const updateDeferred = () => {
+    setDefaultCount(defaultCount + 1);
+    setTransitionCount(defaultCount + 1);
+    setUpdateType("deferred");
+  };
+  const reset = () => {
+    suspenseMap.clear();
+    setMounted(false);
+    setDefaultCount(0);
+    setTransitionCount(0);
+  };
+
+  let count = 0;
+  switch (updateType) {
+    case "update":
+      count = defaultCount;
+      break;
+    case "transition":
+      count = transitionCount;
+      break;
+    case "deferred":
+      count = deferredCount;
+      break;
+  }
+
+  const resolve = () => {
+    const data = suspenseMap.get(suspendedCount);
+    if (data && data.resolvedValue === null) {
+      data.resolvedValue = suspendedCount;
+      data.resolver(suspendedCount);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="App">
+      <div className="Row">
+        <button disabled={mounted} onClick={mount}>
+          Mount
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button disabled={!mounted} onClick={update}>
+          Update
+        </button>
+        <button disabled={!mounted} onClick={updateTransition}>
+          Update (startTransition)
+        </button>
+        <button disabled={!mounted} onClick={updateDeferred}>
+          Update (useDeferredValue)
+        </button>
+        :
+        <button disabled={!mounted} onClick={reset}>
+          Reset
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <div className="ComponentRow">
+        {mounted && (
+          <Suspense fallback={<Fallback />}>
+            <Component count={count} />
+          </Suspense>
+        )}
+      </div>
+
+      <div className="Row">
+        <button onClick={resolve}>Resolve pending</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+function Component({ count }: { count: number }) {
+  let value;
+  try {
+    value = fauxSuspense(count);
+  } catch (thrown) {
+    // Side effect but just for demo
+    suspendedCount = count;
+    throw thrown;
+  }
+
+  return <div className="Component">Component ({value})</div>;
+}
+
+function Fallback() {
+  return <div className="Fallback">Loading...</div>;
+}
+
+const suspenseMap: Map<
+  number,
+  {
+    promise: Promise<number>;
+    resolvedValue: number | null;
+    resolver: (value: number) => void;
+  }
+> = new Map();
+
+let suspendedCount = -1;
+
+function fauxSuspense(value: number): number {
+  let data = suspenseMap.get(value);
+  if (data == null) {
+    let resolver: (value: number) => void;
+    data = {
+      resolvedValue: null,
+      resolver: (value) => resolver(value),
+      promise: new Promise((resolve) => {
+        resolver = resolve;
+      }),
+    };
+
+    suspenseMap.set(value, data);
+  }
+
+  if (data.resolvedValue !== null) {
+    return data.resolvedValue;
+  } else {
+    throw data.promise;
+  }
+}
